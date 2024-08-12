e5a56696e447d393016902ad6a291a1a59f4fec2
RF2034 <namachocoeclairsand@gmail.com>
2024-08-12 23:01:14 +0900

    blogコンテンツ表示修正

--- src/components/BlogContent.tsx
+++ src/components/BlogContent.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC, useEffect, useRef } from "react";
 import parse, {
   domToReact,
   Element,
   attributesToProps,
   HTMLReactParserOptions,
+  DOMNode,
 } from "html-react-parser";
 import Image from "next/image";
 import DOMPurify from "isomorphic-dompurify";
@@ -17,49 +18,44 @@ interface BlogContentProps {
 
 const sanitizeAndOptimizeContent = (htmlContent: string): string => {
   return DOMPurify.sanitize(htmlContent, {
-    ADD_TAGS: ["iframe"],
-    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "scrolling"],
-    FORBID_ATTR: ["style"],
-    FORBID_TAGS: ["script"],
+    ADD_TAGS: ["iframe", "script"],
+    ADD_ATTR: [
+      "allow",
+      "allowfullscreen",
+      "frameborder",
+      "scrolling",
+      "style",
+      "src",
+      "class",
+    ],
+    FORBID_TAGS: [],
+    // ALLOW_SCRIPT_CONTENT: trueを削除
   });
 };
 
 const BlogContent: FC<BlogContentProps> = ({ content }) => {
+  const contentRef = useRef<HTMLDivElement>(null);
   const sanitizedContent = sanitizeAndOptimizeContent(content);
 
   useEffect(() => {
-    // Twitter widgets
-    const twitterScript = document.createElement("script");
-    twitterScript.src = "https://platform.twitter.com/widgets.js";
-    twitterScript.async = true;
-    document.body.appendChild(twitterScript);
-
-    // iframely
-    const iframelyScript = document.createElement("script");
-    iframelyScript.src = "https://cdn.iframe.ly/embed.js";
-    iframelyScript.async = true;
-    document.body.appendChild(iframelyScript);
-
-    return () => {
-      document.body.removeChild(twitterScript);
-      document.body.removeChild(iframelyScript);
-    };
-  }, []);
+    if (contentRef.current) {
+      const scripts = contentRef.current.getElementsByTagName("script");
+      Array.from(scripts).forEach((script) => {
+        const newScript = document.createElement("script");
+        Array.from(script.attributes).forEach((attr) =>
+          newScript.setAttribute(attr.name, attr.value)
+        );
+        newScript.appendChild(document.createTextNode(script.innerHTML));
+        script.parentNode?.replaceChild(newScript, script);
+      });
+    }
+  }, [sanitizedContent]);
 
   const options: HTMLReactParserOptions = {
     replace: (domNode) => {
       if (domNode instanceof Element) {
-        if (domNode.name === "iframe") {
-          const { src, ...props } = domNode.attribs;
-          return (
-            <div className="relative w-full pt-[56.25%]">
-              <iframe
-                className="w-full h-full absolute top-0 left-0"
-                src={src}
-                {...props}
-              />
-            </div>
-          );
+        if (domNode.name === "iframe" || domNode.name === "script") {
+          return domNode;
         }
         if (domNode.name === "img") {
           const { src, alt, width, height, ...rest } = domNode.attribs;
@@ -83,13 +79,19 @@ const BlogContent: FC<BlogContentProps> = ({ content }) => {
     if (isValidElement(parsedContent)) {
       return parsedContent;
     } else if (Array.isArray(parsedContent)) {
-      return domToReact(parsedContent as unknown as Element[]);
+      return domToReact(parsedContent as unknown as DOMNode[]);
     } else {
       return parsedContent as ReactNode;
     }
   };
 
-  return <div className="blog-content">{renderContent()}</div>;
+  return (
+    <div
+      className="blog-content"
+      ref={contentRef}
+      dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+    />
+  );
 };
 
 export default BlogContent;
