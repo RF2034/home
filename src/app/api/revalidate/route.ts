3f0c1142abe3f1309ef4a74996c2a8e431ae56df
RF2034 <namachocoeclairsand@gmail.com>
2024-08-12 20:16:06 +0900

    console.log削除

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,24 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
 export async function POST(request: NextRequest) {
-  console.log("WebHook received:", new Date().toISOString());
+  // console.log("WebHook received:", new Date().toISOString());
 
   const secret = request.nextUrl.searchParams.get("secret");
-  console.log("Received secret:", secret);
+  // console.log("Received secret:", secret);
 
   if (secret !== process.env.REVALIDATE_SECRET) {
-    console.log("Invalid secret");
+    // console.log("Invalid secret");
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
   try {
-    console.log("Revalidating paths");
+    // console.log("Revalidating paths");
     revalidatePath("/blogs");
     revalidatePath("/blogs/[blogId]");
-    console.log("Revalidation complete");
+    // console.log("Revalidation complete");
     return NextResponse.json({ revalidated: true, now: Date.now() });
   } catch (err) {
-    console.error("Revalidation error:", err);
+    // console.error("Revalidation error:", err);
     return NextResponse.json(
       { message: "Error revalidating" },
       { status: 500 }
