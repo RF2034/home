efa455a08c6508cda8b4fcf30818ba1877ef7de7
RF2034 <namachocoeclairsand@gmail.com>
2024-08-12 10:01:15 +0900

    シークレットが丸出しだったので修正

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -3,19 +3,25 @@ import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get("secret");
-  const path = request.nextUrl.searchParams.get("path");
+  const secret = request.headers.get("x-revalidate-secret");
+  const blogId = request.headers.get("x-blog-id");
 
   if (secret !== process.env.REVALIDATE_SECRET) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
 
-  if (!path) {
-    return NextResponse.json({ message: "Path is required" }, { status: 400 });
+  if (!blogId) {
+    return NextResponse.json(
+      { message: "BlogId is required" },
+      { status: 400 }
+    );
   }
 
   try {
-    revalidatePath(path);
+    // ブログ一覧ページを再検証
+    revalidatePath("/blogs");
+    // 特定の記事ページを再検証
+    revalidatePath(`/blogs/${blogId}`);
     return NextResponse.json({ revalidated: true, now: Date.now() });
   } catch (err) {
     return NextResponse.json(
