4cc5227b94ed07d587648d95a590b8921d0d1279
RF2034 <namachocoeclairsand@gmail.com>
2024-08-12 10:54:46 +0900

    表示修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,8 @@ const Footer = () => (
       <div className="flex flex-wrap justify-between">
         <div className="w-full md:w-1/3 mb-6 md:mb-0">
           <h3 className="text-lg font-semibold mb-2">About</h3>
-          <p>キモオタの日記です</p>
+          <p>cigareのブログです</p>
+          <p>日記とかドールさんとか音楽とか</p>
         </div>
         <div className="w-full md:w-1/3">
           <h3 className="text-lg font-semibold mb-2">Connect</h3>
